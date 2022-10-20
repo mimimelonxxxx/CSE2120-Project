9bc4139671229eab5395808a90cc8aa20268ee03
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-10-20 15:37:43 -0600

    worked on more of the periodic table stuffs

--- periodic_table.py
+++ periodic_table.py
@@ -1,32 +1,93 @@
 # First row, First column
-FIRSTFIRST = {
-    "H2": 2.02, 
-    "Li": 6.94, 
-    "Na": 22.99, 
-    "K": 39.10, 
-    "Rb": 85.47, 
-    "Cs": 132.91, 
-    "Fr": 223, 
-    "NH4": 18.05, 
-    "NO3": 62.01, 
-    "ClO3": 83.45, 
-    "CH3COO": 59.05
-}
+FIRSTFIRST = (
+    ("H2", 2.02, 1), 
+    ("Li", 6.94, 1),  
+    ("Na", 22.99, 1), 
+    ("K", 39.10, 1), 
+    ("Rb", 85.47, 1), 
+    ("Cs", 132.91, 1), 
+    ("Fr", 223, 1),
+    ("NH4", 18.05, 1),
+    ("NO3", 62.01, -1),
+    ("ClO3", 83.45, -1),
+    ("CH3COO", 59.05, -1)
+)
 
 # First row, Second column 
-FIRSTSECOND = {
-    "F": 19.00
-}
+FIRSTSECOND = (
+    ("F", 19.00, -1)
+)
 
 # First row, Third column 
-FIRSTTHIRD = {
-    "Cl2": 70.90, 
-    "Br": 79.90, 
-    "I2": 126.90
-}
+FIRSTTHIRD = (
+    ("Cl2", 70.90, -1), 
+    ("Br", 79.90, -1), 
+    ("I2", 126.90, -1)
+)
 
 # First row, Fourth column 
 
-FIRSTFOURTH = { 
-    "SO4": 
-}
+FIRSTFOURTH = (
+    ("SO4", 96.07, -2)
+)
+
+# First row, Fifth column 
+FIRSTFIFTH = (
+    ("CO3", 60.01, -2), 
+    ("PO4", 94.97, -3), 
+    ("SO3", 80.07, -2)
+)
+
+# First row, Sixth column 
+FIRSTSIXTH = (
+    ("IO3", 174.90, -1), 
+    ("OOCCOO", 88.02, -2), 
+)
+
+# First row, Seventh column 
+FIRSTSEVENTH = (
+    ("OH", 17.01, -1)
+)
+
+# Second row, Fifth column 
+SECONDFIFTH = (
+    ("H2", 2.02, 1), 
+    ("Li", 6.94, 1),  
+    ("Na", 22.99, 1), 
+    ("K", 39.10, 1), 
+    ("Rb", 85.47, 1), 
+    ("Cs", 132.91, 1), 
+    ("Fr", 223, 1),
+    ("NH4", 18.05, 1)
+)
+
+# Second row, Sixth column 
+SECONDSIXTH = (
+    ("H2", 2.02, 1), 
+    ("Li", 6.94, 1),  
+    ("Na", 22.99, 1), 
+    ("K", 39.10, 1), 
+    ("Rb", 85.47, 1), 
+    ("Cs", 132.91, 1), 
+    ("Fr", 223, 1),
+    ("NH4", 18.05, 1),
+    ("Co", 58.93, 2), # remember to check if the other reactant is IO3 otherwise forms a precipitate
+    ("Fe", 55.85, 3) # check if it's OOCCOO
+)
+
+# Second row, Seventh column 
+SECONDSEVENTH = (
+    ("H2", 2.02, 1), 
+    ("Li", 6.94, 1),  
+    ("Na", 22.99, 1), 
+    ("K", 39.10, 1), 
+    ("Rb", 85.47, 1), 
+    ("Cs", 132.91, 1), 
+    ("Fr", 223, 1),
+    ("NH4", 18.05, 1)
+)
+
+# Third row, First column 
+THIRDFIRST = ( 
+    ("Rb", )
+)
