e83d94130ec86193b291e05778532071a1e6df97
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-10-20 10:17:43 -0600

    finished flowchart and started periodic table

---
+++ periodic_table.py
@@ -0,0 +1,32 @@
+# First row, First column
+FIRSTFIRST = {
+    "H2": 2.02, 
+    "Li": 6.94, 
+    "Na": 22.99, 
+    "K": 39.10, 
+    "Rb": 85.47, 
+    "Cs": 132.91, 
+    "Fr": 223, 
+    "NH4": 18.05, 
+    "NO3": 62.01, 
+    "ClO3": 83.45, 
+    "CH3COO": 59.05
+}
+
+# First row, Second column 
+FIRSTSECOND = {
+    "F": 19.00
+}
+
+# First row, Third column 
+FIRSTTHIRD = {
+    "Cl2": 70.90, 
+    "Br": 79.90, 
+    "I2": 126.90
+}
+
+# First row, Fourth column 
+
+FIRSTFOURTH = { 
+    "SO4": 
+}
